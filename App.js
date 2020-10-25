a840d0ed0541043e5dc5e4bfe74a4437818b841d
Денис <denisselyanin@gmail.com>
2020-10-25 21:59:05 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+const task1 = {
+  id: 1,
+  name: "task1",
+  description: "to do 1",
+  completed: true
+};
+
+const task2 = {
+  id: 2,
+  name: "task2",
+  description: "to do 2",
+  completed: true
+};
+
+const task3 = {
+  id: 3,
+  name: "task3",
+  description: "to do 3",
+  completed: true
+}
+
+const task4 = {
+  id: 4,
+  name: "task4",
+  description: "to do 4",
+  completed: false
+}
+
+const task5 = {
+  id: 5,
+  name: "task5",
+  description: "to do 5",
+  completed: true
+}
+
+const Item = ({ task }) => {
+  const GetStat  = () => {
+    console.log("Task", task.id, "completed status =", task.completed)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Task">
+      <div className="Name">{task.name}</div>
+      <div className="Description">{task.description}</div>
+      {task.completed ? <div className="Completed">completed</div> : <div className="Uncompleted">uncompleted</div>}
+      <button className="Button" onClick={GetStat}>Get Status</button>
     </div>
-  );
+  )
+}
+
+class MyToDoList extends React.Component {
+  state = {
+    tasks: [task1, task2, task3, task4, task5]
+ }
+ render() {
+   return (
+     <div>
+       {this.state.tasks.map(it => <Item task={it}/>)}
+     </div>
+   )
+ }
+}
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <MyToDoList/>
+      </div>
+    );
+  }
 }
 
 export default App;
