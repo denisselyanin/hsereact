e8af1c33010647ed119a362d1d8124b6629de9d4
Денис <denisselyanin@gmail.com>
2020-12-14 23:55:44 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { ToDoList } from './TaskList.jsx'
 
+import { taskReducer } from './taskReducer.js'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+
+const store = createStore(taskReducer)
+
 class App extends React.Component {
   render(){
     return (
-      <div>
+      <Provider store={store}>
         <ToDoList/>
-      </div>
+      </Provider>
     );
   }
 }
